48cff99ef65a345be7861df077dc8da8f8c55f0b
Malhar9 <71712810+Malhar9@users.noreply.github.com>
2021-01-28 10:11:21 +0530

    Update tree.js

--- tree.js
+++ tree.js
@@ -7,7 +7,7 @@ class Tree{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.image = loadImage("sprites/base.png");
+        this.image = loadImage("tree.png");
         World.add(world, this.body);
       }
       display(){
